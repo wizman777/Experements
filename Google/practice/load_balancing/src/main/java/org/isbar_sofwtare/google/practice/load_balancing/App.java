e4bbe3dce3c0b9391aa7705a905caac1ce2d8e52
Dmitrij Kudriavcev <dmitrij@kudriavcev.info>
2015-04-11 08:08:30 +1000

    Added more google practice tasks

---
+++ Google/practice/load_balancing/src/main/java/org/isbar_sofwtare/google/practice/load_balancing/App.java
@@ -0,0 +1,68 @@
+package org.isbar_sofwtare.google.practice.load_balancing;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class App {
+	public static final String INPUT_FILE_NAME = "input.txt";
+	public static final String OUTPUT_FILE_NAME = "output.txt";
+	
+	public static void main(String[] args) {
+		long beginTime = System.currentTimeMillis();
+		
+		try {
+			String inputName = INPUT_FILE_NAME;
+			String outputName = OUTPUT_FILE_NAME;
+		
+			if (args.length > 0)
+				inputName = args[0];
+		
+			if (args.length > 1)
+				outputName = args[1];
+		
+			try (BufferedReader reader = new BufferedReader(new FileReader(new File(inputName)))) {
+				try (PrintWriter writer = new PrintWriter(outputName)) {
+					int nCases = Integer.parseInt(reader.readLine());
+				
+					for (int nCase = 0; nCase < nCases; ++nCase) {
+						String[] items = reader.readLine().split(" ");
+						
+						long total = 0;
+						
+						long min = Integer.parseInt(items[0]);
+						long max = Integer.parseInt(items[1]);
+						long factor = Integer.parseInt(items[2]);
+					
+						//System.out.println("min: " + min + ", max: " + max + ", factor: " + factor);
+						
+						if (min * factor < max) {
+							long counter = 0;
+						
+							for (long test = min * factor; test < max; test = test * factor) 
+								++counter;
+						
+							if (counter > 0) {
+								++total;
+							
+								while (counter > 1) {
+									counter = (long) ((float) counter / 2.0);
+									++total;
+								}
+							}
+						}
+						
+						writer.println("Case #" + (nCase + 1) + ": " + total);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println(String.format("Done. Spend %d ms", endTime - beginTime));
+	}
+}
---
+++ Google/practice/ropes/src/main/java/org/isbar_software/google/practice/ropes/App.java
@@ -0,0 +1,86 @@
+package org.isbar_software.google.practice.ropes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class App {
+	public static final String INPUT_FILE_NAME = "input.txt";
+	public static final String OUTPUT_FILE_NAME = "output.txt";
+	
+	public static class Rope {
+		private final int x1;
+		private final int x2;
+		
+		public Rope(int x1, int x2) {
+			this.x1 = x1;
+			this.x2 = x2;
+		}
+
+		public int getX1() {
+			return x1;
+		}
+
+		public int getX2() {
+			return x2;
+		}
+		
+		public boolean isIntercept(Rope rope) {
+			return ((x1 - rope.x1) * (x2 - rope.x2)) < 0;
+		}
+
+		@Override
+		public String toString() {
+			return "Rope [x1=" + x1 + ", x2=" + x2 + "]";
+		}
+	}
+	
+	public static void main(String[] args) {
+		long beginTime = System.currentTimeMillis();
+		
+		try {
+			String inputName = INPUT_FILE_NAME;
+			String outputName = OUTPUT_FILE_NAME;
+		
+			if (args.length > 0)
+				inputName = args[0];
+		
+			if (args.length > 1)
+				outputName = args[1];
+		
+			try (BufferedReader reader = new BufferedReader(new FileReader(new File(inputName)))) {
+				try (PrintWriter writer = new PrintWriter(outputName)) {
+					int nCases = Integer.parseInt(reader.readLine());
+				
+					for (int nCase = 0; nCase < nCases; ++nCase) {
+						int nRopes = Integer.parseInt(reader.readLine());
+						
+						Rope[] ropes = new Rope[nRopes];
+						for (int nRope = 0; nRope < nRopes; ++nRope) {
+							String[] items = reader.readLine().split(" ");
+							ropes[nRope] = new Rope(Integer.parseInt(items[0]), Integer.parseInt(items[1]));
+						}
+						
+						int intercept = 0;
+						
+						for (int nRope1 = 0; nRope1 < nRopes; ++nRope1) 
+							for (int nRope2 = nRope1; nRope2 < nRopes; ++nRope2) {
+								if (ropes[nRope1].isIntercept(ropes[nRope2]))
+									++intercept;
+							}
+						
+						writer.println("Case #" + (nCase + 1) + ": " + intercept);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println(String.format("Done. Spend %d ms", endTime - beginTime));
+	}
+
+}
